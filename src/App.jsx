3a0130df0b13f36fd2b21c742a54c7f4ef111edd
Shubleen-digi <shubleen.kaur@digimantra.com>
2025-02-14 12:01:51 +0530

    animation

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// App.js
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./App.css";
+
+const Sparkle = () => {
+  return <div className="sparkle"></div>;
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <Sparkle />
+      <h1>animation,</h1>
+      <p>did you like it!</p>
+    </div>
+  );
+};
+
+export default App;
